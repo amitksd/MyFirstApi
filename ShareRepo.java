5f1f7716b71d8f3d0b4a7be747be4bee5505bb95
amitksd <121122030+amitksd@users.noreply.github.com>
2022-12-23 11:43:35 +0530

    Create ShareRepo.java

---
+++ ShareRepo.java
@@ -0,0 +1,18 @@
+
+public class ShareRepo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hii!! Amit Bhai ,How are you");
+
+		System.out.println("This is my first git hub code Kindly add something From your Side");
+
+	
+	System.out.println("done my side thankyou bhai");
+	
+	
+	
+	
+	}
+
+}
